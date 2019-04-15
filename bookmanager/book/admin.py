785ff541af38d71f59adc120899b260bb8b59f1d
JavsShao <javs_shao@163.com>
2019-04-15 16:00:13 +0800

    Admin站点_调整列表页展示

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -4,10 +4,30 @@ from book.models import *
 
 # Register your models here.
 class BookInfoAdmin(admin.ModelAdmin):
+
+    # 页数大小
     list_per_page = 2
+    # 操作选项的位置
+    actions_on_top = True
+    actions_on_bottom = True
+    # 列表中的列
+    list_display = ['id', 'name', 'title', 'readcount', 'commentcount']
+
+@admin.register(PeopleInfo)
+class PeopleInfoAdmin(admin.ModelAdmin):
+
+    # 列的展示
+    list_display = ['id', 'name', 'readcount']
+
+    # 右侧栏过滤器
+    list_filter = ['book', 'gender']
+
+    # 搜索框
+    search_fields = ['name']
+
 
 
 admin.site.register(BookInfo, BookInfoAdmin)
-admin.site.register(PeopleInfo)
+# admin.site.register(PeopleInfo)
 
 
--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -5,17 +5,24 @@ from django.db import models
 
 # 准备书籍列表信息的模型类
 class BookInfo(models.Model):
-
     # 创建字段，字段类型...
     name = models.CharField(max_length=20, verbose_name='名称')
     pub_date = models.DateField(verbose_name='发布日期',null=True)
     readcount = models.IntegerField(default=0, verbose_name='阅读量')
     commentcount = models.IntegerField(default=0, verbose_name='评论量')
     is_delete = models.BooleanField(default=False, verbose_name='逻辑删除')
+    # image = models.ImageField(upload_to='book',null=True,verbose_name='图片')
 
     class Meta:
         db_table = 'bookinfo'  # 指明数据库表名
         verbose_name = '图书'  # 在admin站点中显示的名称
+
+    #定义一个函数
+    def title(self):
+
+        return "<<" + self.name + ">>"
+
+    title.short_description = '书名'
 
     def __str__(self):
         """定义每个数据对象的显示信息"""
@@ -36,6 +43,10 @@ class PeopleInfo(models.Model):
     class Meta:
         db_table = 'peopleinfo'
         verbose_name = '人物信息'
+
+    def readcount(self):
+        return self.book.readcount
+    readcount.short_description = '图书阅读量'
 
     def __str__(self):
         return self.name
