abe32b4d20467686a5ddf9f9ab61aca02028d321
JavsShao <javs_shao@163.com>
2019-04-15 16:33:34 +0800

    Admin站点_调整编辑页展示

--- bookmanager/book/admin.py
+++ bookmanager/book/admin.py
@@ -3,15 +3,7 @@ from django.contrib import admin
 from book.models import *
 
 # Register your models here.
-class BookInfoAdmin(admin.ModelAdmin):
 
-    # 页数大小
-    list_per_page = 2
-    # 操作选项的位置
-    actions_on_top = True
-    actions_on_bottom = True
-    # 列表中的列
-    list_display = ['id', 'name', 'title', 'readcount', 'commentcount']
 
 @admin.register(PeopleInfo)
 class PeopleInfoAdmin(admin.ModelAdmin):
@@ -24,6 +16,32 @@ class PeopleInfoAdmin(admin.ModelAdmin):
 
     # 搜索框
     search_fields = ['name']
+
+class PeopleInfoTabularInline(admin.TabularInline):
+    model = PeopleInfo  # 要关联的类型
+    extra = 2   # 附加编辑的数量
+
+class BookInfoAdmin(admin.ModelAdmin):
+
+    # 页数大小
+    list_per_page = 2
+    # 操作选项的位置
+    actions_on_top = True
+    actions_on_bottom = True
+    # 列表中的列
+    list_display = ['id', 'name', 'title', 'readcount', 'commentcount']
+
+    # fields = ['name', 'pub_date']
+    fieldsets = (
+        ('基本', {'fields':['name', 'pub_date']}),
+        ('高级',{
+            'fields':['readcount', 'commentcount'],
+            'classes':('collapse',) # 是否折叠显示
+        })
+    )
+
+    inlines = [PeopleInfoTabularInline]
+
 
 
 
